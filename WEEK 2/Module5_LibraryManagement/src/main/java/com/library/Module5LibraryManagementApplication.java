71257f4d1950dacc320f6eccb8f9434d7639e5e1
Meghana Jajula <jajulameghana836@gmail.com>
2026-06-26 16:28:01 +0530

    Add files via upload

---
+++ WEEK 2/Module5_LibraryManagement/src/main/java/com/library/Module5LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module5LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module5LibraryManagementApplication.class, args);
+	}
+
+}
---
+++ WEEK 2/Module5_LibraryManagement/src/main/java/com/library/entity/Book.java
@@ -0,0 +1,37 @@
+package com.library.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    private Long id;
+
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
